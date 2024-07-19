391e48a57d2980e475967f6364f048f0143571c9
viejo bucanero <lopezrunco@gmail.com>
2024-07-19 14:20:35 +0530

    feat: Add real estate page (Blog).

---
+++ template-parts/agenda.php
@@ -0,0 +1,15 @@
+<div class="agenda">
+    <h4>Interesado?</h4>
+    <div class="info">
+        <a href="https://api.whatsapp.com/send/?phone=59899856005&text=Hola%2C+quisiera+agendar+una+visita+a+la+propiedad+<?php echo urlencode($post_title); ?>&type=phone_number&app_absent=0">
+            <b>+598 99 856 005</b>
+        </a>
+        <a href="mailto:administracion@carlosbachino.com">
+            administracion@carlosbachino.com
+        </a>
+    </div>
+    <a class="btn btn-primary" href="https://api.whatsapp.com/send/?phone=59899856005&text=Hola%2C+quisiera+agendar+una+visita+a+la+propiedad+<?php echo urlencode($post_title); ?>&type=phone_number&app_absent=0">
+        Agendar una visita.
+        <i class="fa-solid fa-calendar-days"></i>
+    </a>
+</div>
--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="post my-5">
         <div class="media">
-            <img width="200px" class="mr-3 img-fluid post-thumb d-none d-md-flex border-radius" src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="image">
+            <img width="200px" class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="image">
             <div class="media-body">
                 <h3 class="title mb-1">
                     <a href="<?php the_permalink(); ?>">
