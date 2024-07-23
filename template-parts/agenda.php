d04e5fb03c43d9801d43bf0c16c9d183046728a2
viejo bucanero <lopezrunco@gmail.com>
2024-07-23 16:14:04 +0530

    refactor: Update wapp link to target blank in agenda component.

--- template-parts/agenda.php
+++ template-parts/agenda.php
@@ -8,7 +8,7 @@
             administracion@carlosbachino.com
         </a>
     </div>
-    <a class="btn btn-primary" href="https://api.whatsapp.com/send/?phone=59899856005&text=Hola%2C+quisiera+agendar+una+visita+a+la+propiedad+<?php echo urlencode($post_title); ?>&type=phone_number&app_absent=0">
+    <a class="btn btn-primary" href="https://api.whatsapp.com/send/?phone=59899856005&text=Hola%2C+quisiera+agendar+una+visita+a+la+propiedad+<?php echo urlencode($post_title); ?>&type=phone_number&app_absent=0" target="_blank">
         Agendar una visita.
         <i class="fa-solid fa-calendar-days"></i>
     </a>
