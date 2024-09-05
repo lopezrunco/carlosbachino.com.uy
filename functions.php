8821f183b8a31314501107d47ddf4aca6fe6136c
viejo bucanero <lopezrunco@gmail.com>
2024-09-05 13:44:16 +0530

    refactor: Abstract services data.

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ define('STAFF_DATA_PATH', get_template_directory() . '/data/staff.json');
 define('COMPANY_DATA_PATH', get_template_directory() . '/data/company.json');
 define('OFFICES_DATA_PATH', get_template_directory() . '/data/offices.json');
 define('VENUES_DATA_PATH', get_template_directory() . '/data/venues.json');
+define('SERVICES_DATA_PATH', get_template_directory() . '/data/services.json');
 
 function starterwptheme_theme_support()
 {
